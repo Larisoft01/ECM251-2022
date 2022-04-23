1d41700b0dd08dc8893a53d327b49f1710461302
Larisoft <81933900+Larisoft01@users.noreply.github.com>
2022-04-23 19:35:15 -0300

    Add files via upload

---
+++ ECM251-2022/Aula 3 -Intro POO/introPOO/src/Caneta.java
@@ -0,0 +1,12 @@
+public class Caneta{
+    //Características da caneta - seus atributos
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+
+    //Comportamentos das canetas - seus Métodos
+    void escrever(String texto){
+        System.out.println(texto);
+    }
+}
